ac3a86a63e3927b1f44efdee581b997d7160bf6e
Simone <simone@vizio360.co.uk>
2011-11-10 21:52:32 +0000

    Initial Commit

---
+++ SpiderWars/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  SpiderWars
+//
+//  Created by Simone Vicentini on 10/11/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
